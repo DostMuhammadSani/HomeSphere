c2557e392f182a1499fae9cebf6f9d89af2a2dac
DostMuhammadSani <dostmuhammadsani@gmail.com>
2024-09-18 08:38:28 +0500

    String Changed

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb041d0d941a71575a081222e304ef5b88d0260")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b8a7246cecad53b7cdb2a5a186850332956296f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

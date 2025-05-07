10bef1c153cc4db3877f87eaf045b3560f044341
DostMuhammadSani <dostmuhammadsani@gmail.com>
2025-05-08 02:00:58 +0500

    Financial Module 66 Percent + Orders Fixed

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ebb87deb5f46341ff9cd111f4ed42c3f36878f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45e21f6e9a75c8bb3757e1d52a30094868bb4791")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
